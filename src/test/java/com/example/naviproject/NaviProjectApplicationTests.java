24d83a726ea5ed4cd201960b1dadba8bac7e115b
강지원 <kangji149@naver.com>
2023-07-31 21:20:09 +0900

    Initial Commit

---
+++ src/test/java/com/example/naviproject/NaviProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.naviproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaviProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
