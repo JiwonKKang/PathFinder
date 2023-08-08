86831da7d18189e7ac91dd6d22123d43515b544b
강지원 <kangji149@naver.com>
2023-08-08 09:21:39 +0900

    pharmacy and direction data setup

--- src/main/java/com/example/naviproject/NaviProjectApplication.java
+++ src/main/java/com/example/naviproject/NaviProjectApplication.java
@@ -2,8 +2,10 @@ package com.example.naviproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class NaviProjectApplication {
 
 	public static void main(String[] args) {
