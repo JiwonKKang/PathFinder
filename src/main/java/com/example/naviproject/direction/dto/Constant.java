835617bc7c5400e4c81377c4628c892404230fe7
Jiwon <kangji149@naver.com>
2023-08-10 23:22:19 +0900

    카카오맵 길안내를 이용해 길안내 URI 제공 및 로드뷰 URI 제공 (#9)

---
+++ src/main/java/com/example/naviproject/direction/dto/Constant.java
@@ -0,0 +1,6 @@
+package com.example.naviproject.direction.dto;
+
+public class Constant {
+    public static final String ROAD_VIEW_BASE_URL = "https://map.kakao.com/link/roadview/";
+    public static final String DIRECTION_BASE_URL = "https://map.kakao.com/link/map/";
+}
